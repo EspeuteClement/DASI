059211b395677629970d88b883fb463567e41f2d
capellev <Mordoron58>
2016-03-11 15:45:48 +0100

    Message

--- Collectif/src/main/java/fr/insalyon/dasi/collectif/presentation/Main.java
+++ Collectif/src/main/java/fr/insalyon/dasi/collectif/presentation/Main.java
@@ -1,7 +1,13 @@
 package fr.insalyon.dasi.collectif.presentation;
 
+import fr.insalyon.dasi.collectif.service.ServiceMetier;
+
 public class Main {
     public static void main(String[] args) {
+        
+        ServiceMetier serviceMetier = new ServiceMetier();
+        
+        serviceMetier.inscrireAdherent("Capelle", "Victor", "36 Rue", "capellev.info@gmail.com", "admin");
         
     }
 }
